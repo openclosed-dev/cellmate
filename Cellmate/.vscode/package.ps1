2a1baa9ad7be975f781fb884e334b4d0dee1c38e
leadpony <hacker@leadpony.org>
2020-03-17 21:18:06 +0900

    Move projects to repository root

--- Cellmate/.vscode/package.ps1
+++ Cellmate/.vscode/package.ps1
@@ -5,8 +5,8 @@ $outdir = "bin\$name\$version"
 
 dotnet publish -c Release -o $outdir
 
-Copy-Item -Path ..\..\LICENSE -Destination $outdir
-Copy-Item -Path ..\..\CHANGELOG.md -Destination $outdir
+Copy-Item -Path ..\LICENSE -Destination $outdir
+Copy-Item -Path ..\CHANGELOG.md -Destination $outdir
 Copy-Item -Path .\Cellmate.psd1 -Destination $outdir
 
 Compress-Archive -Path bin\$name -DestinationPath bin\$name-$version.zip
