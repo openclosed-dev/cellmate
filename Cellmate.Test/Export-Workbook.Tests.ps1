52a3834f8e95d3893e2ba5d3fd406497f9194bee
leadpony <leadpony@openclosed.dev>
2024-02-25 23:48:56 +0900

    Fix test case for Export-Workbook

--- Cellmate.Test/Export-Workbook.Tests.ps1
+++ Cellmate.Test/Export-Workbook.Tests.ps1
@@ -8,42 +8,42 @@ BeforeAll {
 Describe "Export-Workbook" {
 
     It "exports as default" {
-        Get-Item "Export-Workbook-1.csv" | 
+        Get-Item "Export-Workbook-1.csv" |
             Import-Workbook |
             Export-Workbook -Destination $result > $null
-            
-        "$result\Export-Workbook-1.xlsx" | Should -Exist 
+
+        "$result\Export-Workbook-1.csv" | Should -Exist
     }
 
     It "exports as explicit default" {
-        Get-Item "Export-Workbook-2.csv" | 
+        Get-Item "Export-Workbook-2.csv" |
             Import-Workbook |
             Export-Workbook -As Default -Destination $result > $null
-            
-        "$result\Export-Workbook-2.xlsx" | Should -Exist 
+
+        "$result\Export-Workbook-2.xlsx" | Should -Exist
     }
 
     It "exports as CSV" {
-        Get-Item "Export-Workbook-1.csv" | 
+        Get-Item "Export-Workbook-1.csv" |
             Import-Workbook |
             Export-Workbook -As Csv -Destination $result > $null
-            
+
         "$result\Export-Workbook-1.csv" | Should -Exist
         Get-Content "$result\Export-Workbook-1.csv" |
-            Should -Be (Get-Content "$here\Export-Workbook-1.csv") 
+            Should -Be (Get-Content "$here\Export-Workbook-1.csv")
     }
 
     It "exports as PDF" {
-        Get-Item "Export-Workbook-1.csv" | 
+        Get-Item "Export-Workbook-1.csv" |
             Import-Workbook |
             Export-Workbook -As Pdf -Destination $result > $null
-            
-        "$result\Export-Workbook-1.pdf" | Should -Exist 
+
+        "$result\Export-Workbook-1.pdf" | Should -Exist
     }
 
     It "throws if the format is invalid" {
         {
-            Get-Item "Export-Workbook-1.csv" | 
+            Get-Item "Export-Workbook-1.csv" |
                 Import-Workbook |
                 Export-Workbook -As unknown
         } | Should -Throw
